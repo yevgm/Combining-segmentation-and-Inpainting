491b7748095402db90967a38e097602368992434
Men Yevgeniy <yevgeni.technion@gmail.com>
2022-01-10 22:17:30 +0200

    cosmetics for easier use with command line + config ++

--- config.py
+++ config.py
@@ -18,8 +18,6 @@ def parse_global_args(parent, add_help=False):
     parser.add_argument('--output-dir', default=output_folder, type=str,
                         help='output dir')
 
-    # parser.add_argument('--save-model', default=False, type=bool,
-    #                     help='save model to output folder or not')
     parser.add_argument('--seed', default=42, type=int,
                         help='random seed for everything')
     parser.add_argument('--device', default=device, type=str,
